16d41dcb0814cc89b52d6d0c0b910485a6c8fb09
cpj97 <130832014+cpj97@users.noreply.github.com>
2023-09-15 11:51:35 +0200

    saving the Rscript file

---
+++ my_first_Rscript.R
@@ -0,0 +1,13 @@
+# Set-up your script ------------------------------------------------------
+
+# install.packages(c("tidyverse", "gapminder", "pacman")) # uncomment if already installed
+pacman::p_load(tidyverse, gapminder)
+
+# Load your Data into R ---------------------------------------------------
+
+data(gapminder)
+head(gapminder)
+
+gapminder_clean <- gapminder %>% 
+  rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  mutate(gdp = pop * gdp_per_cap)
